e1323cff80c8da3de855deeb881dfa408b959dae
yozman <yozman@sina.com>
2016-04-03 13:02:33 +0800

    v0.4.3-4

--- config/development/routes.php
+++ config/development/routes.php
@@ -6,7 +6,7 @@ $route['user_guide'] =
         if (is_dir(APPPATH.'public/user_guide'));
 
         else {
-            $target = substr(BASEPATH, 0 ,-7).'user_guide';
+            $target = substr(BASEPATH, 0, -7).'user_guide';
             $link_name = APPPATH.'public/user_guide';
             `ln -s $target $link_name`;
             header('Location: /user_guide');
