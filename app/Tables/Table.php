56bf90fc697ec99715f66f4d5505fd5a06fca262
yozman <yozman@sina.com>
2016-04-12 15:00:43 +0800

    v0.4.12

--- app/Tables/Table.php
+++ app/Tables/Table.php
@@ -1,5 +1,17 @@
 <?php namespace App\Tables;
 
-class Table {
+use Illuminate\Database\Eloquent\Model;
+
+class Table extends Model {
+
+    protected $db;
+    public $itmestamps = FALSE;
+
+    public function __construct () {
+        parent::__construct();
+        $CI =& get_instance();
+        $CI->load->database();
+        $this->db = $CI->db;
+    }
 
 }
--- resources/helpers/functions.php
+++ resources/helpers/functions.php
@@ -2,8 +2,31 @@
 
 if ( ! function_exists('wtf')) {
 
-    function wtf ($_args) {
-        var_dump($_args);
+    function wtf () {
+        echo '<pre style="font:12px/20px Consolas,Monaco,Courier New,Courier,monospace">';
+        var_dump($_input);
         exit;
     }
+}
+
+if ( ! function_exists('useEloquent')) {
+
+    function useEloquent ($_input) {
+
+        $manager = new \Illuminate\Database\Capsule\Manager;
+
+        $manager->addConnection([
+            'driver'    => 'mysql',
+            'host'      => $_input['hostname'],
+            'database'  => $_input['database'],
+            'username'  => $_input['username'],
+            'password'  => $_input['password'],
+            'charset'   => $_input['char_set'],
+            'collation' => $_input['dbcollat'],
+            'prefix'    => $_input['dbprefix']
+        ]);
+
+        $manager->bootEloquent();
+    }
+
 }
