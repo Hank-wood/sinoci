7b09787dbe7cf0afaf5b57c70eacb8e9a5bdb0f9
yozman <yozman@sina.com>
2016-04-12 16:37:44 +0800

    v0.4.12-1

--- config/app.php
+++ config/app.php
@@ -1,22 +1,30 @@
 <?php
 
 // 定义 APPPATH 常量：应用目录
-defined('APPPATH') or define('APPPATH', dirname(__DIR__).'/');
+defined('APPPATH') OR
+    define('APPPATH', dirname(__DIR__).'/');
 
 // 定义 BASEPATH 常量：CI 框架目录
-defined('BASEPATH') or define('BASEPATH', APPPATH.'vendor/codeigniter/framework/system/');
+defined('BASEPATH') OR
+    define('BASEPATH', APPPATH.'vendor/codeigniter/framework/system/');
 
 // 定义 ENVIRONMENT 常量：运行环境
-defined('ENVIRONMENT') or define('ENVIRONMENT', getenv('APP_ENV') ?: 'development');
+defined('ENVIRONMENT') OR
+    define('ENVIRONMENT', getenv('APP_ENV') ?: 'development');
 
 // 定义 VIEWPATH 常量：模版目录
-defined('VIEWPATH') or define('VIEWPATH', APPPATH.'resources/views/');
+defined('VIEWPATH') OR
+    define('VIEWPATH', APPPATH.'resources/views/');
 
 // 设置控制器目录
 $routing['directory'] = ENVIRONMENT;
 
-// 配置报错
-ini_set('display_errors', getenv('APP_DEBUG') OR ENVIRONMENT === 'development');
+// 是否打开调试
+ini_set('display_errors', getenv('APP_DEBUG'));
+
+// 运行环境配置
+file_exists(APPPATH.'config/'.ENVIRONMENT.'/app.php') &&
+    require_once(APPPATH.'config/'.ENVIRONMENT.'/app.php');
 
 // 请求 CI 框架入口
 require_once BASEPATH.'core/CodeIgniter.php';
