d5e243657c1d669c48020b3669e628f68240be14
yozman <yozman@sina.com>
2016-04-20 01:12:32 +0800

    v0.4.20-3

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -10,9 +10,21 @@ class Controller extends CI_Controller {
 			->set_output($_input);
 	}
 
+    public function get ($_key, $_value) {
+        return isset($_GET[$_key]) ?
+            $_GET[$_key] :
+            $_value;
+    }
+
+    public function post ($_key, $_value) {
+        return isset($_POST[$_key]) ?
+            $_POST[$_key] :
+            $_value;
+    }
+
 	public function __destruct () {
 		if ($this->_output) {
-			$this->json($this->_output);
+			$this->json($this->_output)->_display();
 		}
 	}
 
