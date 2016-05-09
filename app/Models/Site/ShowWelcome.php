2e0aff926e6505fcf89e727fafa6c96a5e87d378
yozman <yozman@sina.com>
2016-05-10 01:28:10 +0800

    v0.5.10-3

--- app/Models/Site/ShowWelcome.php
+++ app/Models/Site/ShowWelcome.php
@@ -9,29 +9,6 @@ trait ShowWelcome {
         // 添加视图目录
         $this->load->add_package_path(dirname(BASEPATH).'/application/');
 
-        // 设置 $elapsed_time 变量
-        $elapsed_time =
-            $this->benchmark->elapsed_time(
-                'loading_time:_base_classes_start',
-                'loading_time:_base_classes_end'
-            );
-
-        // 渲染模版
-        $welcome_message =
-            $this->view(
-                'welcome_message',
-                compact('elapsed_time')
-            );
-
-        // 过滤多余信息并输出
-        // TODO
-        // 1. 过滤正则
-        return
-            preg_replace(
-                '/<p.+\/code>/',
-                '',
-                $welcome_message,
-                1
-            );
+        return $this->view('welcome_message');
     }
 }
