81daf65584102246fa169c509448a694e7fee2e0
yozman <yozman@sina.com>
2016-07-12 11:08:14 +0800

    v0.7.12

--- app/Models/Ddl/Create.php
+++ app/Models/Ddl/Create.php
@@ -7,17 +7,27 @@ use Illuminate\Database\Capsule\Manager;
 trait Create {
     
     function create ($_input) {
-        foreach ($_input as $table => $columns) {
-            Manager::schema()->hasTable($table) OR
-                Manager::schema()->create($table, function ($table) use ($columns) {
-                    $table->increments('id');
-                    $table->timestamps();
-                    $table->softDeletes();
-                    foreach ($columns as $name => $attributes) {
-                        list($name, $attributes[0]) = [$attributes[0], $name];
-                        call_user_func_array([$table, $name], $attributes);
-                    }
-                });
+        array_filter($_input, [ $this, '_createTable' ], true);
+    }
+
+    private function _createTable ($_columns, $_table) {
+        Manager::schema()->hasTable($_table) OR
+            Manager::schema()->create($_table, function ($table) use ($_columns) {
+                $this->_createColumns($table, $_columns);
+                $table->increments('id');
+                $table->timestamps();
+                $table->softDeletes();
+            });
+    }
+
+    private function _createColumns ($_table, $_columns) {
+        foreach ($_columns as $name => $attributes) {
+            if (is_string($attributes)) {
+                $attributes = explode(' ', $attributes);
+            }
+            list($name, $attributes[0]) = [ $attributes[0], $name ];
+            call_user_func_array([ $_table, $name ], $attributes);
         }
     }
+
 }
