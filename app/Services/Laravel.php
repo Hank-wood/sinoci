031cc5c76ab059bac6dce28688c4f33074bc6a67
木由子 <yozman@sina.com>
2016-08-01 18:13:46 +0800

    使用单个容器

--- app/Services/Laravel.php
+++ app/Services/Laravel.php
@@ -4,15 +4,28 @@ namespace App\Services;
 
 use Illuminate\Database\Capsule\Manager;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Container\Container;
 
 class Laravel
 {
 
+    private static $container;
+
     public function __construct()
     {
+        // 初始化容器
+        self::$container = $this->getContainer();
+
+        // 循环开启功能
         array_filter(config('use_laravel'), function ($v) {
             call_user_func([$this, 'use' . $v]);
         });
+    }
+
+    protected function getContainer()
+    {
+        // 返回单个容器
+        return self::$container ?: new Container;
     }
 
     public function useBlade()
@@ -31,9 +44,10 @@ class Laravel
         });
 
         // 初始化 Eloquent
-        $manager = new Manager;
+        $manager = new Manager(self::$container);
         $manager->addConnection(config('db'));
         $manager->setAsGlobal();
         $manager->bootEloquent();
     }
+
 }
