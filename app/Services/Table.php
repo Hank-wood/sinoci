57f54bdbe1b84fa6e21a7449ecbade1b4645e590
yozman <yozman@sina.com>
2016-08-15 23:36:50 +0800

    拓展 eloquent 添加 like or like

--- app/Services/Table.php
+++ app/Services/Table.php
@@ -9,4 +9,24 @@ class Table extends Model
 
     public $timestamps = false;
 
+    public function scopeLike($query, $key, $val)
+    {
+        return $query->where($key, 'like', "%{$val}%");
+    }
+
+    public function scopeLikeRaw($query, $key, $val)
+    {
+        return $query->whereRaw("{$key} like '%{$val}%'");
+    }
+
+    public function scopeOrLike($query, $key, $val)
+    {
+        return $query->orWhere($key, 'like', "%{$val}%");
+    }
+
+    public function scopeOrLikeRaw($query, $key, $val)
+    {
+        return $query->orWhereRaw("{$key} like '%{$val}%'");
+    }
+
 }
